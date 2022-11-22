8742a6dc454ebbe859975762cf268559648946a9
CircumFlexing Coon <baumgaertner997@gmx.de>
2022-11-22 19:53:54 +0100

    Java core
    new Java core prefs

--- LibrarbyManagementSystem/src/main/java/swt/hse/de/JfxConnector.java
+++ LibrarbyManagementSystem/src/main/java/swt/hse/de/JfxConnector.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
